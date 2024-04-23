be7c2eb5e7b2713439c2b364a28c8d25be763ab1
Huzaifa Rizwan <huzaifa.rizwan1231@gmail.com>
2024-04-23 19:09:40 +0500

    Added Add reimbursement form

--- src/pages/Reimbursement.jsx
+++ src/pages/Reimbursement.jsx
@@ -1,22 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReimbursementTable from '../components/ReimbursementTable'
+import NewReimbursement from '../components/NewReimbursement'
 
 export default function Reimbursement() {
+
+  const [showForm, setShowForm] = useState(false);
+  
   return (
-    <div className="main">
-        <div className="main-wrapper">
-            <div className="page-content">
-               <div className="page-header flex items-center justify-between">
-                <div>
-                    <h6><b>Reimbursement Management</b></h6>
+   
+    <>
+      <div className="main">
+          <div className="main-wrapper">
+              <div className="page-content">
+                <div className="page-header flex items-center justify-between">
+                  <div>
+                      <h6><b>Reimbursement Management</b></h6>
+                  </div>
+                  <button className="button reimbursement-button" onClick={()=>{
+                    setShowForm(true);
+                  }}>+ Add Reimbursement</button>
                 </div>
-                <button className="button reimbursement-button">+ Add Reimbursement</button>
-               </div>
-               <div className="page-table">
-                    <ReimbursementTable/>
-               </div>
-            </div>
-        </div>
-    </div>
+                <div className="page-table">
+                      <ReimbursementTable/>
+                </div>
+              </div>
+          </div>
+      </div>
+      <NewReimbursement showForm={showForm} setShowForm={setShowForm}/>
+    </>
   )
 }
--- src/pages/Resource.jsx
+++ src/pages/Resource.jsx
@@ -1,24 +1,28 @@
-import React from 'react'
-import ResourceTable from '../components/ResourceTable'
+import React from "react";
+import ResourceTable from "../components/ResourceTable";
 
 export default function Resource() {
   return (
     <>
       <div className="main">
         <div className="main-wrapper">
-            <div className="page-content">
-               <div className="page-header flex items-center justify-between">
-                <div>
-                    <h6><b>Resource Allocation</b></h6>
-                </div>
-                <button className="button reimbursement-button">+ Add Resource</button>
-               </div>
-               <div className="page-table">
-                    <ResourceTable/>
-               </div>
+          <div className="page-content">
+            <div className="page-header flex items-center justify-between">
+              <div>
+                <h6>
+                  <b>Resource Allocation</b>
+                </h6>
+              </div>
+              <button className="button reimbursement-button">
+                + Add Resource
+              </button>
             </div>
+            <div className="page-table">
+              <ResourceTable />
+            </div>
+          </div>
         </div>
-    </div>
+      </div>
     </>
-  )
+  );
 }
